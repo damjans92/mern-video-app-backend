54db45eaf47c25c5fa30da3d8d1b2b7eb0659e93
Damjan Savkovic <damjans92@gmail.com>
2023-11-08 17:38:16 +0100

    verifyToken fix

--- verifyToken.js
+++ verifyToken.js
@@ -6,43 +6,44 @@ export const verifyToken = (req, res, next) => {
   const refreshToken = req.cookies.refresh_token
 
   if (!accessToken) {
-    return next(createError(401, 'You are not authenticated'))
+    if (refreshToken) {
+      // Attempt to refresh the access token using the refresh token
+      jwt.verify(refreshToken, process.env.JWT, (err, user) => {
+        if (err) {
+          return next(
+            createError(403, 'Access and refresh tokens are not valid.')
+          )
+        }
+
+        // Refresh token is valid. Issue a new access token.
+        const newAccessToken = jwt.sign({ id: user._id }, process.env.JWT, {
+          expiresIn: '10m',
+        })
+
+        res.cookie('access_token', newAccessToken, {
+          httpOnly: true,
+          sameSite: 'none',
+          secure: true,
+          maxAge: 10 * 60 * 1000,
+          // Other cookie options
+        })
+
+        // Continue processing the request with the new access token.
+        req.user = user
+        return next()
+      })
+    } else {
+      return next(createError(401, 'You are not authenticated'))
+    }
   }
 
   jwt.verify(accessToken, process.env.JWT, (err, user) => {
     if (err) {
-      if (refreshToken) {
-        jwt.verify(refreshToken, process.env.JWT, (err, user) => {
-          if (err) {
-            return next(
-              createError(403, 'Access and refresh tokens are not valid.')
-            )
-          }
-
-          // Refresh token is valid. Issue a new access token.
-          const newAccessToken = jwt.sign({ id: user._id }, process.env.JWT, {
-            expiresIn: '10m',
-          })
-
-          res.cookie('access_token', newAccessToken, {
-            httpOnly: true,
-            sameSite: 'none',
-            secure: true,
-            maxAge: 10 * 60 * 1000,
-            // Other cookie options
-          })
-
-          // Continue processing the request with the new access token.
-          req.user = user
-          next()
-        })
-      } else {
-        return next(createError(403, 'Access token is not valid.'))
-      }
-    } else {
-      // Access token is valid. Continue processing the request.
-      req.user = user
-      next()
+      return next(createError(403, 'Access token is not valid.'))
     }
+
+    // Access token is valid. Continue processing the request.
+    req.user = user
+    next()
   })
 }
